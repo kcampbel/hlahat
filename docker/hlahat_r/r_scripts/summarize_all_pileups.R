ff4d1cf49889b500c44c4c577113799518a0180c
Katie C <katiecampbell@mednet.ucla.edu>
2021-11-30 16:42:54 -0800

    fix all pileups for ref included

--- docker/hlahat_r/r_scripts/summarize_all_pileups.R
+++ docker/hlahat_r/r_scripts/summarize_all_pileups.R
@@ -10,8 +10,9 @@ output_prefix <- args[2]
 
 # read_bed <- fread(bed_file, sep = "\t", col.names = c("CHROM", "START", "END", "INFO"))
 read_pileups <- fread(pileups_file, col.names = c('CHROM','POS','REF','DEPTH','BASES','BQ_ASCII'))
+read_pileups <- fread("~/hlahat_final/working/tm_dev/SKCM-3N-A9WB-NB.pileups.txt", col.names = c('CHROM','POS','REF','DEPTH','BASES','BQ_ASCII'))
 
-summBases <- function(test) {
+summBases <- function(test, ref_base) {
   counts <- rep(0, 6)
   names(counts) <- c("A", "T", "C", "G", "N", "Other")
   if(is.na(test)) {
@@ -20,7 +21,10 @@ summBases <- function(test) {
     i = 1
     while (i < nchar(test)+1){
       this_char <- toupper(substring(test, i, i))
-      if (this_char %in% c('A','T','C','G','N')) {
+      if (this_char %in% c('.', ',')) {
+        counts[ref_base] <- counts[ref_base] + 1
+        i = i + 1
+      } else if (this_char %in% c('A','T','C','G','N')) {
         counts[this_char] <- counts[this_char] + 1
         i = i + 1
       } else if (this_char %in% c("-", "+")){
@@ -33,14 +37,14 @@ summBases <- function(test) {
       } else if (this_char %in% c("$", "*", ">", "<")) {
         i = i + 1
       } else {
-        print(test)
         i = i + 1
       }
     }
     return(counts)
   }
 }
-summ_pileups <- read_pileups %>% group_by(CHROM, POS) %>% mutate(summBase = list(summBases(BASES))) %>% unnest_wider(summBase) %>% mutate(BASES = NULL, BQ_ASCII = NULL)
+summ_pileups <- read_pileups %>% group_by(CHROM, POS) %>% 
+  mutate(summBase = list(summBases(BASES, REF))) %>% unnest_wider(summBase) %>% mutate(BASES = NULL, BQ_ASCII = NULL)
 
 # final_pileups <- inner_join(read_bed, summ_pileups, by = c("CHROM" = "CHROM", "END" = "POS"))
 write.table(summ_pileups, file = paste0(output_prefix, ".summarize_pileups.txt"), sep = "\t", quote = F, row.names = F)
